085763c8df35fdbc447d4921968cc204e9e9a70e
Adina Cohen <adina.cohen2@mail.yu.edu>
2019-08-01 21:57:06 -0400

    Working with combining the tfidf and bag of words vectors

--- teshuva_classification/sgdc.py
+++ teshuva_classification/sgdc.py
@@ -6,6 +6,20 @@ from sklearn.model_selection import train_test_split
 
 print ("loading")
 
+print ("loading")
+import os
+inputpath = 'testclassifier/'
+outputpath = 'responsa'
+
+classnames = []
+for dirpath, dirnames, filenames in os.walk(inputpath):
+    structure = os.path.join(outputpath, dirpath[len(inputpath):])
+    classnames.append(dirnames)
+    if not os.path.isdir(structure):
+        os.mkdir(structure)
+
+print(classnames)
+classnames = classnames[0]
 resp = datasets.load_files('testclassifier/', encoding='utf-8')
 
 train_data, eval_data, train_target, eval_target = train_test_split(resp.data, resp.target, test_size=0.1)
@@ -33,40 +47,53 @@ for word in f:
 
 # loop through the data and create a vector for the bag of words
 # keep track of which vector corresponds to which bit of data
+
+# adding to the train vectors
+new_vtrain_data = []
 dataNum = 0
 rows, cols = vtrain_data.shape
 for row in range(rows):
     for entry in vtrain_data[row]:
-        # entry = numpy.array(entry)
-        # print(entry.shape)
         sent = train_data[dataNum]
         words = sent.split(" ")
-        bagVector = numpy.zeros(len(bagOfWords))
+        bagVector = numpy.zeros(len(bagOfWords), dtype=object)
         for word in words:
             for i, w in enumerate(bagOfWords):
                 if word == w:
                     bagVector[i] += 1
-        entry = numpy.append(entry, bagVector, axis=50000)
-        # print(entry.shape)
+        # vtrain_data[row].data = numpy.append(vtrain_data[row].data, bagVector)
+        new_vtrain_data.append(numpy.append(vtrain_data[row].data, bagVector)) # take the bag of words vector and add it back to the tfidf vector
         dataNum += 1
+new_vtrain_data = numpy.asarray(new_vtrain_data)
 
-# for file in train_data:
-#     for sent in file:
-#         sent = sent.strip.split()
-#         bagVector = numpy.zeros(len(bagOfWords))
-#         for word in sent:
-#             for i, w in enumerate(bagOfWords):
-#                 if word == w:
-#                     bagVector[i] += 1
+# adding to the test vectors
+new_vtest_data = []
+dataNum = 0
+rows, cols = vtest_data.shape
+for row in range(rows):
+    for entry in vtest_data[row]:
+        sent = eval_data[dataNum]
+        words = sent.split(" ")
+        bagVector = numpy.zeros(len(bagOfWords), dtype=object)
+        for word in words:
+            for i, w in enumerate(bagOfWords):
+                if word == w:
+                    bagVector[i] += 1
+        # vtest_data[row].data = numpy.append(vtest_data[row].data, bagVector)
+        new_vtest_data.append(numpy.append(vtest_data[row].data, bagVector)) # add the bag of words vector to the tfidf vector
+        dataNum += 1
+new_vtest_data = numpy.asarray(new_vtest_data)
 
+# TODO: create a function that will transform all new data into vectors that include the bag of words
+# TODO: change the vectors being used in x and y to be the new vectors that include BoW
 
 clf = MultinomialNB()
-clf.fit(vtrain_data, train_target)
+clf.fit(new_vtrain_data, train_target)
 
 print('fitted, scoring:')
 
-x = clf.score(vtrain_data, train_target)
-y = clf.score(vtest_data, eval_target)
+x = clf.score(new_vtrain_data, train_target)
+y = clf.score(new_vtest_data, eval_target)
 
 print (x, y)
 print ("done")
@@ -75,10 +102,34 @@ print ("done")
 test_responsa = datasets.load_files('responsa/', encoding='utf-8')
 test_responsa_data = test_responsa.data
 test_responsa_target = test_responsa.target
-test_responsa_data = tfidf.transform(test_responsa_data)
-y = clf.score(test_responsa_data, test_responsa_target)
+test_responsa_data2 = tfidf.transform(test_responsa_data)
+
+# adding vectors to the test responsa - i.e continued transformation of the data
+new_test_responsa_data = []
+dataNum = 0
+rows, cols = test_responsa_data2.shape
+for row in range(rows):
+    for entry in test_responsa_data2[row]:
+        # entry = numpy.array(entry)
+        # print(entry.shape)
+        sent = test_responsa_data[dataNum]
+        words = sent.split(" ")
+        bagVector = numpy.zeros(len(bagOfWords))
+        for word in words:
+            for i, w in enumerate(bagOfWords):
+                if word == w:
+                    bagVector[i] += 1
+        # test_responsa_data2[row].data = numpy.append(test_responsa_data2[row].data, bagVector)
+        new_test_responsa_data.append(numpy.append(test_responsa_data2[row].data, bagVector))
+        dataNum += 1
+
+y = clf.score(new_test_responsa_data, test_responsa_target)
 print(y)
 
-test = clf.predict(test_responsa_data)
+test = clf.predict(test_responsa_data2)
 print(test)
+
+for responsum, guess, answer in zip(test_responsa.data, test, test_responsa_target):
+    print(responsum)
+    print(classnames[guess], classnames[answer])
 
