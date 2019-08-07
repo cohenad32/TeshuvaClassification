8e591688d519f18701bddf59eea3f7b04c311395
Adina Cohen <adina.cohen2@mail.yu.edu>
2019-08-06 22:34:34 -0400

    Added new responsa

--- getTestResponsa.py
+++ getTestResponsa.py
@@ -4,10 +4,18 @@ client = MongoClient()
 
 db = client.sefaria.texts
 
-TeshuvotMaharshal = db.find_one({"versionTitle": "Teshuvot Maharshal, Lublin, 1574"})
-test = TeshuvotMaharshal["chapter"][26]
+# TeshuvotMaharshal = db.find_one({"versionTitle": "Teshuvot Maharshal, Lublin, 1574"})
+# test = TeshuvotMaharshal["chapter"][26]
+#
+# file = open("testResponsaShabbos2.txt", "w", encoding='utf-16')
+# for line in test:
+#     file.write(line + "\n")
+# file.close()
 
-file = open("testResponsaShabbos2.txt", "w", encoding='utf-16')
+NBY = db.find_one({"title": "Noda BiYhudah I"})
+test = NBY["chapter"]["Orach Chaim"][2]
+
+file = open("NBYprayer.txt", "w", encoding='utf-8')
 for line in test:
     file.write(line + "\n")
 file.close()
--- teshuva_classification/sgdc.py
+++ teshuva_classification/sgdc.py
@@ -2,6 +2,8 @@ import numpy
 from sklearn import datasets
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier, Perceptron, LogisticRegression
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.model_selection import train_test_split
 
 print ("loading")
@@ -20,17 +22,17 @@ for dirpath, dirnames, filenames in os.walk(inputpath):
 
 print(classnames)
 classnames = classnames[0]
+classnames.sort()
 resp = datasets.load_files('testclassifier/', encoding='utf-8')
 
 train_data, eval_data, train_target, eval_target = train_test_split(resp.data, resp.target, test_size=0.1)
-
 print ("transform")
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=1, norm=None,
     encoding='utf-16', ngram_range=(1, 2), token_pattern=r'''(?u)\b\w[\w'"]+\b''', max_features=50000
     )
 
 vtrain_data = tfidf.fit_transform(train_data)
-vtrain_data = vtrain_data.todense() # convert the vector to a dense list so that additional features can be added on later
+vtrain_data = vtrain_data.todense()
 print('fit_transform complete')
 vtest_data = tfidf.transform(eval_data)
 vtest_data = vtest_data.todense()
@@ -41,11 +43,23 @@ print ("tuning")
 # and extract features from that and then add it to the corresponding vtrain_data.data
 
 # import citation bag of words
-f = open("/Users/adinacohen/Documents/GitHub/TeshuvaClassification/CRFSample-master/citationBagOfWords").readlines()
+f = open("../CRFSample-master/citationBagOfWords", encoding='utf-8').readlines()
 bagOfWords = []
 for word in f:
     word = word.strip("\n")
     bagOfWords.append(word)
+
+bow = set()
+from nltk import ngrams
+for phrase in bagOfWords:
+    phrase = phrase.replace(')', ') ')
+    phrase = phrase.split()
+    for i in range(1, len(phrase)+1):
+        for n in ngrams(phrase, i):
+            bow.add(' '.join(n))
+
+print(len(bow), bow)
+bagOfWords = list(bow)
 
 # loop through the data and create a vector for the bag of words
 # keep track of which vector corresponds to which bit of data
@@ -57,18 +71,19 @@ rows, cols = vtrain_data.shape
 for row in range(rows):
     for entry in vtrain_data[row]:
         sent = train_data[dataNum]
-        words = sent.split(" ")
+        sent = sent.split(" ")
         bagVector = numpy.zeros(len(bagOfWords), dtype=object)
-        for word in words:
-            for i, w in enumerate(bagOfWords):
-                if word == w:
-                    bagVector[i] += 1
-        # vtrain_data[row] = numpy.append(vtrain_data[row], bagVector)
-        # new_vtrain_data.append(numpy.append(vtrain_data[row].tolist(), bagVector)) # take the bag of words vector and add it back to the tfidf vector
-        new_vtrain_data.append(vtrain_data[row].tolist())
-        new_vtrain_data.append(bagVector.tolist())
+        for i, w in enumerate(bagOfWords):
+            if w in sent:
+                bagVector[i] += 1
+        # for word in words:
+        #     for i, w in enumerate(bagOfWords):
+        #         if word == w:
+        #             bagVector[i] += 1
+        #vtrain_data[row].data = numpy.append(vtrain_data[row].data, bagVector)
+        new_vtrain_data.append(numpy.append(vtrain_data[row].data, bagVector)) # take the bag of words vector and add it back to the tfidf vector
         dataNum += 1
-new_vtrain_data = numpy.asarray(new_vtrain_data).reshape(rows, cols + len(bagOfWords))
+new_vtrain_data = numpy.asarray(new_vtrain_data)
 
 # adding to the test vectors
 new_vtest_data = []
@@ -77,27 +92,28 @@ rows, cols = vtest_data.shape
 for row in range(rows):
     for entry in vtest_data[row]:
         sent = eval_data[dataNum]
-        words = sent.split(" ")
+        sent  = sent.split(" ")
         bagVector = numpy.zeros(len(bagOfWords), dtype=object)
-        for word in words:
-            for i, w in enumerate(bagOfWords):
-                if word == w:
-                    bagVector[i] += 1
-        vtest_data[row] = numpy.append(vtest_data[row], bagVector)
-        # new_vtest_data.append(numpy.append(vtest_data[row].data, bagVector)) # add the bag of words vector to the tfidf vector
+        for i, w in enumerate(bagOfWords):
+            if w in sent:
+                bagVector[i] += 1
+        #vtest_data[row].data = numpy.append(vtest_data[row].data, bagVector)
+        new_vtest_data.append(numpy.append(vtest_data[row].data, bagVector)) # add the bag of words vector to the tfidf vector
         dataNum += 1
-# new_vtest_data = numpy.asarray(new_vtest_data)
+new_vtest_data = numpy.asarray(new_vtest_data)
 
 # TODO: create a function that will transform all new data into vectors that include the bag of words
 # TODO: change the vectors being used in x and y to be the new vectors that include BoW
 
-clf = MultinomialNB()
-clf.fit(vtrain_data, train_target)
+clf = SGDClassifier(penalty='elasticnet')
+#clf = MultinomialNB()
+#clf = LogisticRegression()
+clf.fit(new_vtrain_data, train_target)
 
 print('fitted, scoring:')
 
-x = clf.score(vtrain_data, train_target)
-y = clf.score(vtest_data, eval_target)
+x = clf.score(new_vtrain_data, train_target)
+y = clf.score(new_vtest_data, eval_target)
 
 print (x, y)
 print ("done")
@@ -109,7 +125,6 @@ test_responsa_target = test_responsa.target
 test_responsa_data2 = tfidf.transform(test_responsa_data)
 test_responsa_data2 = test_responsa_data2.todense()
 
-
 # adding vectors to the test responsa - i.e continued transformation of the data
 new_test_responsa_data = []
 dataNum = 0
@@ -119,23 +134,31 @@ for row in range(rows):
         # entry = numpy.array(entry)
         # print(entry.shape)
         sent = test_responsa_data[dataNum]
-        words = sent.split(" ")
+        sent = sent.split(" ")
         bagVector = numpy.zeros(len(bagOfWords))
-        for word in words:
-            for i, w in enumerate(bagOfWords):
-                if word == w:
-                    bagVector[i] += 1
-        test_responsa_data2[row] = numpy.append(test_responsa_data2[row], bagVector)
-        # new_test_responsa_data.append(numpy.append(test_responsa_data2[row].data, bagVector))
+        for i, w in enumerate(bagOfWords):
+            if w in sent:
+                bagVector[i] += 1
+        # test_responsa_data2[row].data = numpy.append(test_responsa_data2[row].data, bagVector)
+        new_test_responsa_data.append(numpy.append(test_responsa_data2[row].data, bagVector))
         dataNum += 1
 
+new_test_responsa_data = numpy.asarray(new_test_responsa_data)
 y = clf.score(new_test_responsa_data, test_responsa_target)
 print(y)
+#
+# n = 3
+# probs = clf.predict_proba(new_test_responsa_data)
+# best_n = numpy.argsort(probs, axis=1)[:,-n:]
+#
+# for responsum, guess, answer in zip(test_responsa.data, best_n, test_responsa_target):
+#     print(responsum)
+#     print(classnames[answer])
+#     print(classnames[guess[0]], classnames[guess[1]], classnames[guess[2]])
 
-test = clf.predict(test_responsa_data2)
+test = clf.predict(new_test_responsa_data)
 print(test)
 
 for responsum, guess, answer in zip(test_responsa.data, test, test_responsa_target):
     print(responsum)
     print(classnames[guess], classnames[answer])
-
